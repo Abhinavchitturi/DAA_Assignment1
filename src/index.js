50048f5436b5cd91e6ca7dcd41708a101aa0adfd
Abhinav Chitturi <f20220064@hyderabad.bits-pilani.ac.in>
2025-03-24 01:13:29 +0530

    Fixed homepage path

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; // Fixed incorrect import
+import { BrowserRouter } from "react-router-dom"; // Added BrowserRouter for routing fix
 import App from "./App";
 import "katex/dist/katex.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/DAA_Assignment1"> 
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
